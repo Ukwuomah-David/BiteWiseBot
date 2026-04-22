2b951cf83212b530e5661be68b44904f8c3dd64a
David <ukwuomahdavid@gmail.com>
2026-04-22 08:37:58 +0100

    Webhook fix

--- webhook.py
+++ webhook.py
@@ -105,4 +105,5 @@ def payment_success():
 # RUN SERVER
 # =========================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
